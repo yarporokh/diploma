a7fa97a6382c6e30277a7dc68a061593ac999a9b
yarporokh <yaroslavporohnenko@gmail.com>
2024-02-26 15:13:16 +0200

    added user repository

---
+++ src/main/java/com/nau/shop/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nau.shop.repository;
+
+import com.nau.shop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
