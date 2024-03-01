ecf12adc8893295791602535eb2f127a3169a9f8
yarporokh <yaroslavporohnenko@gmail.com>
2024-03-01 23:09:52 +0200

    create main controller

---
+++ src/main/java/com/nau/shop/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.nau.shop.controller;
+
+import com.nau.shop.dto.RegisterBody;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping()
+    public String mainPage(Model model, HttpSession session) {
+        model.addAttribute("registerBody", new RegisterBody());
+
+        Boolean isNew = (Boolean) session.getAttribute("isNew");
+        model.addAttribute("isNew", isNew);
+
+        session.removeAttribute("isNew");
+        return "main";
+    }
+
+}
