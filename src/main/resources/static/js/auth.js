d510c492b94b8a8789238b70360f906cffda6c86
yarporokh <yaroslavporohnenko@gmail.com>
2024-04-05 21:24:53 +0300

    added styles

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -1,3 +1,21 @@
+(function () {
+    'use strict'
+
+    window.addEventListener('load', function () {
+        var forms = document.getElementsByClassName('needs-validation')
+
+        Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+    }, false)
+}())
+
 function openRegisterModal() {
     $('#loginModal').modal('hide');
     $('#registerModal').modal('show');
@@ -7,57 +25,6 @@ function openLoginModal() {
     $('#registerModal').modal('hide');
     $('#loginModal').modal('show');
 }
-/*
-function registerNewUser() {
-    let firstname = document.getElementById("firstname").value
-    let lastname = document.getElementById("lastname").value
-    let email = document.getElementById("email").value
-    let password = document.getElementById("password").value
-    let phone = document.getElementById("phone").value
 
-    // let phoneRegex = /^0\d{9}$/
-    if (firstname === ""
-        || lastname === ""
-        || email === ""
-        || password === ""
-        || phone === ""
-       /!* || !phoneRegex.test(phone)*!/) {
-        return
-    }
 
-    let requestBody = {
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        password: password,
-        phone: phone
-    }
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    }
-
-    let errDiv = document.getElementById("err")
-
-    let uri = "http://localhost:8080/api/v1/auth/register"
-    fetch(uri, requestOptions)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if (data === true) {
-                errDiv.innerHTML = ''
-                errDiv.innerHTML += "<span>Юзер успешно зареган</span>"
-                alert("Юзер успешно зарега")
-            } else {
-                errDiv.innerHTML = ''
-                errDiv.innerHTML += "<span>Юзер уже зареган</span>"
-                alert("Юзер уже зареган")
-            }
-        })
-
-}*/
 
