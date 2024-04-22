b5747632cfc3f2448f8d2c88994603d4566c0c42
yarporokh <yaroslavporohnenko@gmail.com>
2024-04-22 11:50:32 +0300

    remove useless imports

--- src/main/java/com/nau/shop/controller/rest/AuthRestController.java
+++ src/main/java/com/nau/shop/controller/rest/AuthRestController.java
@@ -4,11 +4,11 @@ import com.nau.shop.dto.RegisterBody;
 import com.nau.shop.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// TODO: delete/edit
 
 @RestController
 @RequestMapping("/api/v1/auth")
